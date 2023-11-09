c737bf1b70d1ac279f467fd910ab233105f590bb
crdigital <clayton.metal2@hotmail.com>
2023-11-09 11:32:46 -0300

    Realizado as tarefas do Desafio com sucesso! :)

--- TestesUnitarios.Desafio.Console/Program.cs
+++ TestesUnitarios.Desafio.Console/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TestesUnitarios.Desafio.Console.Services;
+
+ValidacoesLista vl = new ValidacoesLista();
+
+var listaMista = new List<int> { 5, -1, -8, 9 };
+
+List<int> listaPositivos = vl.RemoverNumerosNegativos(listaMista);
+
+foreach(int num in listaPositivos)
+{
+    Console.WriteLine(num);
+}
--- TestesUnitarios.Desafio.Tests/ValidacoesListaTests.cs
+++ TestesUnitarios.Desafio.Tests/ValidacoesListaTests.cs
@@ -44,23 +44,28 @@ public class ValidacoesListaTests
         var numeroParaProcurar = 10;
 
         // Act
+        bool resultado = _validacoes.ListaContemDeterminadoNumero(lista, numeroParaProcurar);   
 
         // Assert
+        Assert.False(resultado);
     }
 
     //TODO: Corrigir a anotação [Fact]
+    [Fact]
     public void DeveMultiplicarOsElementosDaListaPor2()
     {
         //TODO: Implementar método de teste
-
+        
         // Arrange
         var lista = new List<int> { 5, 7, 8, 9 };
         var resultadoEsperado = new List<int> { 10, 14, 16, 18 };
         
         // Act
+        var resultadoTeste = _validacoes.MultiplicarNumerosLista(lista, 2);
 
         // Assert
-    }
+        Assert.Equal(resultadoTeste, resultadoEsperado);
+    }   
 
     [Fact]
     public void DeveRetornar9ComoMaiorNumeroDaLista()
@@ -71,10 +76,11 @@ public class ValidacoesListaTests
         var lista = new List<int> { 5, -1, -8, 9 };
 
         // Act
+        int retorno = _validacoes.RetornarMaiorNumeroLista(lista);
 
         // Assert
         //TODO: Corrigir o Assert.Equal com base no retorno da chamada ao método
-        Assert.Equal(9, 9);
+        Assert.Equal(9, retorno);
     }
 
     [Fact]
@@ -90,6 +96,6 @@ public class ValidacoesListaTests
 
         // Assert
         //TODO: Corrigir o Assert.Equal com base no retorno da chamada ao método
-        Assert.Equal(-8, -8);
+        Assert.Equal(-8, resultado);
     }
 }
